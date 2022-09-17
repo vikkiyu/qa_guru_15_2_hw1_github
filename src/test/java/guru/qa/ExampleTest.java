51f4eec77cfceb0bc0dee52684eb98f0aab263a5
Victoria Yu <yuvictoria23@gmail.com>
2022-09-18 04:10:07 +0600

    Init commit from console

---
+++ src/test/java/guru/qa/ExampleTest.java
@@ -0,0 +1,13 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+
+    void exampleTest(){
+        Assertions.assertTrue(2 > 1);
+    }
+}
